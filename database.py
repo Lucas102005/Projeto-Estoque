f73286d93220b8c9bfe06ae942d86db058ac201e
Lucas Gimenez Sarmazo <168141121+Lucas102005@users.noreply.github.com>
2026-04-30 03:38:26 +0000

    Criação de gitignore

--- database.py
+++ database.py
@@ -1,10 +1,14 @@
 import psycopg2
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def conectar():
     return psycopg2.connect(
         host="db.qmpmfxpskjyiseuzuond.supabase.co",
         database="postgres",
         user="postgres",
-        password="r#sg_z%uG?/69Tr",
+        password=os.getenv("DB_PASSWORD"),
         port="5432"
     )
