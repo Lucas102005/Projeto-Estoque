770d54544dcc76ce98221257f6c1973fed2f9a6e
Lucas Gimenez Sarmazo <168141121+Lucas102005@users.noreply.github.com>
2026-04-30 04:09:37 +0000

    Atualização do banco

--- database.py
+++ database.py
@@ -6,9 +6,9 @@ load_dotenv()
 
 def conectar():
     return psycopg2.connect(
-        host="db.qmpmfxpskjyiseuzuond.supabase.co",
-        database="postgres",
-        user="postgres",
+        host=os.getenv("DB_HOST"),
+        database=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASSWORD"),
-        port="5432"
+        port=os.getenv("DB_PORT")
     )
